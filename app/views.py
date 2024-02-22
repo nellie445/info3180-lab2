8b25b215d4fecf6f810b2c406a7fb84031eb0334
nellie445 <nelliesmart445@gmail.com>
2024-02-21 22:40:55 -0500

    Editted existing files and created new ones

--- app/views.py
+++ app/views.py
@@ -1,24 +1,44 @@
+from datetime import datetime
 from app import app
-from flask import render_template, request, redirect, url_for, flash
-
+from flask import Flask,  render_template, request, redirect, url_for, flash
 
 ###
 # Routing for your application.
 ###
 
+
 @app.route('/')
 def home():
-    """Render website's home page."""
-    return render_template('home.html')
+   """Render website's home page."""
+   return render_template('home.html')
+
 
 
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Jhanell Edwards")
+
+@app.route('/profile')
+def  profile():
+    profile_data = {
+        'full_name': 'Jhanell Edwards',
+        'username': 'kalex',
+        'location': 'Kingston',
+        'join_date': '2021, 2, 21',
+        'short_bio': 'As a computer science student, I am fascinated by algorithms and problem-solving, especially in AI and machine learning.Proficient in Python, Java, and C++, I am passionate about developing scalable software solutions. Engaging in coding competitions and collaborative projects, I am dedicated to mastering the dynamic landscape of computer science.',
+        'num_posts': 100,
+        'num_followers': 500,
+        'num_following': 300
+    }
+   
+    return render_template('profile.html', profile_data=profile_data)
 
 
-###
+
+def format_date_joined(date):
+    # Assuming date is a datetime object
+    return date.strftime("%b, %Y")
 # The functions below should be applicable to all Flask apps.
 ###
 
